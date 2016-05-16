7307fd6c1193bebeee3bcc016c58513273e08515
aleksandr106 <igor.glazov1907@gmail.com>
2016-05-16 10:42:57 +0400

    Update G4.py

--- G4.py
+++ G4.py
@@ -1,5 +1,5 @@
 def dekstra(g,start):
-    sp={vertex : float('-inf') for vertex in g }
+    sp={vertex : float('inf') for vertex in g }
     sp[start]=0
     q=[start]
     while q:
@@ -15,7 +15,10 @@ m=int(input())
 k=int(input())
 g={}
 for i in range(m):
-    a,b,w= [float(x) for x in input().split()]
+    s=input().split()
+    a=int(s[0])
+    b=int(s[1])
+    w=float(s[2])
     if a not in g:
         g[a]={b:w}
     else:
@@ -27,5 +30,7 @@ for i in range(m):
 sp=dekstra(g,k)
 for vertex in g:
     print(vertex,sp[vertex])
+
+
 
 
